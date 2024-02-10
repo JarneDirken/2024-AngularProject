9ee2566a724ed75119872d1a8b0870f669841002
Jarne Dirken <r0882161@student.thomasmore.be>
2024-02-10 15:50:48 +0100

    update hosting

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  home_url: "https://angular-23-team-08.vercel.app/",
+  home_url: "https://2024-angular-project.vercel.app/",
   AUTH0_DOMAIN: "dev-2sg17n1hbz7mip76.us.auth0.com",
   AUTH0_CLIENT_ID: "repI3tUYUwYyBnEiDLzpuJFI69E0BHGy",
   AUTH0_AUDIENCE: "https://tripplannerbackendapi.azurewebsites.net/api",
-  redirectUri: "https://angular-23-team-08.vercel.app/",
+  redirectUri: "https://2024-angular-project.vercel.app/",
   api_url: "https://tripplannerbackendapi.azurewebsites.net/api"
 };
